30d2a6db4c43eeaf1d57205ed1ddaab1ce20574d
UNHOLY DEVOTION <unholy@devotion.com>
2026-03-12 19:14:55 -0300

    Update deploy script to upload all folders (COMPLETO, TRACKS, etc.)

--- deploy_hostinger.bat
+++ deploy_hostinger.bat
@@ -3,20 +3,19 @@ setlocal
 
 :: --- FTP CONFIGURATION ---
 set "FTP_HOST=88.223.85.137"
-:: Username and Password will be asked at runtime for security
 set /p "FTP_USER=Introduzca usuario FTP: "
 set /p "FTP_PASS=Introduzca password FTP: "
-:: Use /public_html for Hostinger unless the domain points elsewhere
 set "REMOTE_DIR=/public_html"
 
 :: --- FILE LIST ---
-:: List of files to upload (Add or remove as needed)
-set "FILES=index.html bio.html media.html sets.html tour.html releases.html merch.html about.html press.html contact.html global.css global.js robots.txt sitemap.xml 404.html vercel.json"
+set "FILES=index.html bio.html media.html sets.html tour.html releases.html merch.html about.html press.html contact.html blog.html comments.html global.css global.js robots.txt sitemap.xml 404.html vercel.json"
+
+:: --- FOLDERS TO UPLOAD ---
+set "FOLDERS=COMPLETO TRACKS merch_assets ARTWORKS VIDEOS blog scripts"
 
 echo.
-echo [1/3] Preparando script de carga para %FTP_HOST%...
+echo [1/4] Preparando script de carga para %FTP_HOST%...
 
-:: Create temporary FTP script
 set "FTPSCRIPT=%temp%\unholy_ftp.txt"
 echo open %FTP_HOST% > "%FTPSCRIPT%"
 echo %FTP_USER% >> "%FTPSCRIPT%"
@@ -24,7 +23,7 @@ echo %FTP_PASS% >> "%FTPSCRIPT%"
 echo binary >> "%FTPSCRIPT%"
 echo cd %REMOTE_DIR% >> "%FTPSCRIPT%"
 
-:: Add file uploads
+:: Upload individual files
 for %%f in (%FILES%) do (
     if exist "%%f" (
         echo put "%%f" >> "%FTPSCRIPT%"
@@ -33,13 +32,120 @@ for %%f in (%FILES%) do (
     )
 )
 
+:: Upload folders
+for %%d in (%FOLDERS%) do (
+    if exist "%%d" (
+        echo mkdir %%d >> "%FTPSCRIPT%"
+        echo cd %%d >> "%FTPSCRIPT%"
+        echo lcd %%d >> "%FTPSCRIPT%"
+        echo mput * >> "%FTPSCRIPT%"
+        echo cd .. >> "%FTPSCRIPT%"
+        echo lcd .. >> "%FTPSCRIPT%"
+    ) else (
+        echo [!] Carpeta no encontrada: %%d
+    )
+)
+
 echo quit >> "%FTPSCRIPT%"
 
-echo [2/3] Iniciando transferencia vía FTP (Hostinger)...
+echo [2/4] Iniciando transferencia de archivos...
 ftp -s:"%FTPSCRIPT%"
 
 echo.
-echo [3/3] Limpieza de archivos temporales...
+echo [3/4] Subiendo carpetas (COMPLETO, TRACKS, etc.)...
+
+:: Upload COMPLETO folder
+if exist "COMPLETO" (
+    echo Subiendo COMPLETO...
+    echo open %FTP_HOST% > "%FTPSCRIPT%"
+    echo %FTP_USER% >> "%FTPSCRIPT%"
+    echo %FTP_PASS% >> "%FTPSCRIPT%"
+    echo binary >> "%FTPSCRIPT%"
+    echo cd %REMOTE_DIR% >> "%FTPSCRIPT%"
+    echo mkdir COMPLETO >> "%FTPSCRIPT%"
+    echo cd COMPLETO >> "%FTPSCRIPT%"
+    echo lcd COMPLETO >> "%FTPSCRIPT%"
+    echo mput * >> "%FTPSCRIPT%"
+    echo cd .. >> "%FTPSCRIPT%"
+    echo lcd .. >> "%FTPSCRIPT%"
+    echo quit >> "%FTPSCRIPT%"
+    ftp -s:"%FTPSCRIPT%"
+)
+
+:: Upload TRACKS folder
+if exist "TRACKS" (
+    echo Subiendo TRACKS...
+    echo open %FTP_HOST% > "%FTPSCRIPT%"
+    echo %FTP_USER% >> "%FTPSCRIPT%"
+    echo %FTP_PASS% >> "%FTPSCRIPT%"
+    echo binary >> "%FTPSCRIPT%"
+    echo cd %REMOTE_DIR% >> "%FTPSCRIPT%"
+    echo mkdir TRACKS >> "%FTPSCRIPT%"
+    echo cd TRACKS >> "%FTPSCRIPT%"
+    echo lcd TRACKS >> "%FTPSCRIPT%"
+    echo mput * >> "%FTPSCRIPT%"
+    echo cd .. >> "%FTPSCRIPT%"
+    echo lcd .. >> "%FTPSCRIPT%"
+    echo quit >> "%FTPSCRIPT%"
+    ftp -s:"%FTPSCRIPT%"
+)
+
+:: Upload merch_assets folder
+if exist "merch_assets" (
+    echo Subiendo merch_assets...
+    echo open %FTP_HOST% > "%FTPSCRIPT%"
+    echo %FTP_USER% >> "%FTPSCRIPT%"
+    echo %FTP_PASS% >> "%FTPSCRIPT%"
+    echo binary >> "%FTPSCRIPT%"
+    echo cd %REMOTE_DIR% >> "%FTPSCRIPT%"
+    echo mkdir merch_assets >> "%FTPSCRIPT%"
+    echo cd merch_assets >> "%FTPSCRIPT%"
+    echo lcd merch_assets >> "%FTPSCRIPT%"
+    echo mput * >> "%FTPSCRIPT%"
+    echo cd .. >> "%FTPSCRIPT%"
+    echo lcd .. >> "%FTPSCRIPT%"
+    echo quit >> "%FTPSCRIPT%"
+    ftp -s:"%FTPSCRIPT%"
+)
+
+:: Upload VIDEOS folder
+if exist "VIDEOS" (
+    echo Subiendo VIDEOS...
+    echo open %FTP_HOST% > "%FTPSCRIPT%"
+    echo %FTP_USER% >> "%FTPSCRIPT%"
+    echo %FTP_PASS% >> "%FTPSCRIPT%"
+    echo binary >> "%FTPSCRIPT%"
+    echo cd %REMOTE_DIR% >> "%FTPSCRIPT%"
+    echo mkdir VIDEOS >> "%FTPSCRIPT%"
+    echo cd VIDEOS >> "%FTPSCRIPT%"
+    echo lcd VIDEOS >> "%FTPSCRIPT%"
+    echo mput * >> "%FTPSCRIPT%"
+    echo cd .. >> "%FTPSCRIPT%"
+    echo lcd .. >> "%FTPSCRIPT%"
+    echo quit >> "%FTPSCRIPT%"
+    ftp -s:"%FTPSCRIPT%"
+)
+
+:: Upload blog folder
+if exist "blog" (
+    echo Subiendo blog...
+    echo open %FTP_HOST% > "%FTPSCRIPT%"
+    echo %FTP_USER% >> "%FTPSCRIPT%"
+    echo %FTP_PASS% >> "%FTPSCRIPT%"
+    echo binary >> "%FTPSCRIPT%"
+    echo cd %REMOTE_DIR% >> "%FTPSCRIPT%"
+    echo mkdir blog >> "%FTPSCRIPT%"
+    echo cd blog >> "%FTPSCRIPT%"
+    echo lcd blog >> "%FTPSCRIPT%"
+    echo mput * >> "%FTPSCRIPT%"
+    echo cd .. >> "%FTPSCRIPT%"
+    echo lcd .. >> "%FTPSCRIPT%"
+    echo quit >> "%FTPSCRIPT%"
+    ftp -s:"%FTPSCRIPT%"
+)
+
+echo.
+echo [4/4] Limpiando archivos temporales...
 del "%FTPSCRIPT%"
 
 echo.
